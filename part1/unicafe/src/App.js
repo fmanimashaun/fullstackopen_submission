88a0b188b576d3a2c6bca793510721b0e5f47325
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-08-02 21:23:06 +0100

    complete exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,26 +1,30 @@
 import { useState } from 'react';
 
 const Button = ({ handleclick, text }) => (
-  <button onClick={handleclick}>
-    {text}
-  </button>
+  <button onClick={handleclick}>{text}</button>
 );
 const Display = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const percentagePositive = (good / total) * 100;
   return (
     <div>
       <h2>statistics</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {percentagePositive}%</p>
     </div>
   );
 };
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const handleclick = (value, setter) => () => {
     setter(value + 1);
@@ -35,7 +39,7 @@ const App = () => {
 
       <Display good={good} neutral={neutral} bad={bad} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
