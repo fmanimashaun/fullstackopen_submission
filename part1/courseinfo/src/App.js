28f761febf08fea118ff7201875e261eab6640f9
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-08-02 13:45:57 +0100

    Completion of exercise 1.1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,9 +1,31 @@
-
 const Header = (props) => {
   return (
     <h1>{props.course}</h1>
   );
 };
+
+const Content = (props) => {
+  return (
+    <>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  );
+};
+
 
 
 const App = () => {
@@ -18,6 +40,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
 }
