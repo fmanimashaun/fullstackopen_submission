f690b5576759676f4036fb7e83665a9726bd03d5
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-08-02 23:03:08 +0100

    complete exercise 1.12 to 1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,4 +1,20 @@
-import { useState } from 'react'
+import { useState, useMemo } from 'react';
+
+const Button = ({ handleclick, text }) => {
+  return <button onClick={handleclick}>{text}</button>;
+};
+
+const MostVotesAnecdote = ({ votes, anecdotes }) => {
+  const max = useMemo(() => Math.max(...votes), [votes]);
+  const index = votes.indexOf(Math.max(...votes));
+  return (
+    <div>
+      <h2>Anecdote with most votes</h2>
+      <p>{anecdotes[index]}</p>
+      <p>has {max} {max < 2 ? 'vote' : 'votes'}</p>
+    </div>
+  );
+};
 
 const App = () => {
   const anecdotes = [
@@ -9,16 +25,36 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
+    'The only way to go fast, is to go well.',
+  ];
 
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
+
+  const handleclick = (value, setValue) => {
+    if (setValue === setSelected) {
+      return () => {
+        const random = Math.floor(Math.random() * anecdotes.length);
+        setSelected(random);
+      };
+    } else {
+      return () => {
+        const copy = [...votes];
+        copy[value] +=  1;
+        setVotes(copy);
+      };
+    }
+  };
 
   return (
     <div>
-      {anecdotes[selected]}
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]} {votes[selected]} {votes[selected] < 2 ? 'vote' : 'votes'}</p>
+      <Button handleclick={handleclick(selected, setVotes)} text="vote" />
+      <Button handleclick={handleclick(selected, setSelected)} text="next anecdote" />
+      <MostVotesAnecdote votes={votes} anecdotes={anecdotes} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
